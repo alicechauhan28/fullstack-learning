05f928019f683eda58fb2fa9d87964749c6cfc02
Alice Chauhan <alicechauhan2@gmail.com>
2026-09-09 15:33:53 +0530

    feat: add variables and data types practice

--- csharp-basics/hello-world/Program.cs
+++ csharp-basics/hello-world/Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Hello world my name is Alice:)");
+string name = "Alice";
+int age = 21;
+bool isLearningCSharp = true;
+double height = 5.3;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isLearningCSharp);
+Console.WriteLine(height);
+
+
